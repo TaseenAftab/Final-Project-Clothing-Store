888aa86a733ced69b69400a76c3cd73055a1bf9a
TaseenAftab <t4taseenaftab@gmail.com>
2024-09-21 18:05:15 +0500

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,8 +1,6 @@
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 
-import phonenumbers
-from phonenumbers import NumberParseException
 def login_required():
     if 'user_id' in session:
         return True
